6fec5bfdcd9a8d1c716dade34101451a79372164
guzus <50664161+guzus@users.noreply.github.com>
2022-02-22 00:39:00 +0900

    env variable

--- src/action.py
+++ src/action.py
@@ -2,6 +2,10 @@ import thecampy
 from .reporter import Reporter
 from .postman import Postman
 from .spy import Spy
+import os
+
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
 
 # get list of soldiers from issue
 soldiers = Spy.get_soldiers_from_issue()
@@ -10,4 +14,4 @@ soldiers = Spy.get_soldiers_from_issue()
 letters = Reporter.publish_letters()
 
 # email, pw is from environment variables
-Postman(email=1, password=1).send_letters(letters, soldiers)
+Postman(email=EMAIL, password=PASSWORD).send_letters(letters, soldiers)
