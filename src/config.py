9b3385e9d91aace452183dc8b97a0a12dad33f64
guzus <50664161+guzus@users.noreply.github.com>
2022-03-05 15:27:38 +0900

    change secret key name

--- src/config.py
+++ src/config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 EMAIL = os.environ.get("THE_CAMP_EMAIL")
 PASSWORD = os.environ.get("THE_CAMP_PASSWORD")
-GITHUB_ID = os.environ.get("GITHUB_ID")
-GITHUB_ACCESS_KEY = os.environ.get("GITHUB_ACCESS_KEY")
+GITHUB_ID = os.environ.get("MY_GITHUB_ID")
+GITHUB_ACCESS_KEY = os.environ.get("MY_GITHUB_ACCESS_KEY")
 REPO_URL = "guzus/nonsan-trainee-news"
